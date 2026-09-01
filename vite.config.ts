a5933818f1ac91908632f43ce0b6f7f62de23b05
Taquara em Foco <135270722+dscorrea83@users.noreply.github.com>
2026-09-01 13:35:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
+export default defineConfig({
+  base: "/reclameRGE/",
+  plugins: [react()]
+});
     base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
